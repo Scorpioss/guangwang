f0787e25aca506048449c267bc82b8d02eaf9d2f
NSC <90592216+Scorpioss@users.noreply.github.com>
2024-05-27 02:26:55 -0700

    edit email

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts" generic="T extends any, O extends any">
 defineOptions({
-  name: 'IndexPage',
-})
+  name: "IndexPage",
+});
 </script>
 
 <template>
   <div>
-    <img h-full w-full src="../assets/bg.webp" alt="">
-    <div class="email">可以上架</div>
+    <img h-full w-full src="../assets/bg.webp" alt="" />
+    <div class="email" absolute bottom-10 w-full flex justify-center>
+      <div rd-2 p-2 p-x-5 text-6 color-white class="Funny">
+        madyunkeltz639@gmail.com
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.Funny{
+  background-color: #000;
+}
+</style>
