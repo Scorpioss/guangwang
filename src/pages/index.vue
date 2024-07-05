b379b98544882244e1c311c8b8bb6fdda2aad749
NSC <90592216+Scorpioss@users.noreply.github.com>
2024-07-05 13:33:42 +0800

    change email

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,8 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
-      </div>
+MrLi.yunxiang@icloud.com       </div>
     </div>
   </div>
 </template>
